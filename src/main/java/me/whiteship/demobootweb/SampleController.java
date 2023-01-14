ce09560df5a98aaf0c9e70b19c566da15c83b86f
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-14 13:06:09 +0900

    WebMvcConfigurer 1부 Formatter

---
+++ src/main/java/me/whiteship/demobootweb/SampleController.java
@@ -0,0 +1,14 @@
+package me.whiteship.demobootweb;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @GetMapping("/hello/{name}")
+    public String hello(@PathVariable String name) {
+        return "hello " + name;
+    }
+}
---
+++ src/test/java/me/whiteship/demobootweb/SampleControllerTest.java
@@ -0,0 +1,23 @@
+package me.whiteship.demobootweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@WebMvcTest
+class SampleControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void hello() throws Exception {
+        this.mockMvc.perform(get("/hello/keesun"))
+                .andDo(print())
+                .andExpect(content().string("hello keesun"));
+    }
+}
