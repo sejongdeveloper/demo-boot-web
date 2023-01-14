4ff28a381bd952c1a78bb42c42239c8a111b1d93
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-14 14:49:45 +0900

    리소스 핸들러

--- src/test/java/me/whiteship/demobootweb/SampleControllerTest.java
+++ src/test/java/me/whiteship/demobootweb/SampleControllerTest.java
@@ -4,13 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.web.servlet.MockMvc;
+
+import javax.servlet.http.HttpServlet;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -35,9 +37,11 @@ class SampleControllerTest {
 
     @Test
     void helloStatic() throws Exception {
-        this.mockMvc.perform(get("/index.html"))
+        this.mockMvc.perform(get("/mobile/index.html"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().string(containsString("hello index")));
+                .andExpect(content().string(containsString("hello mobile")))
+                .andExpect(header().exists(HttpHeaders.CACHE_CONTROL))
+        ;
     }
 }
