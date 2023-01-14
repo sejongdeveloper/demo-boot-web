705cd809c0705c0a515a2b7079fe093a7898031a
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-14 13:59:37 +0900

    리소스 핸들러

--- src/test/java/me/whiteship/demobootweb/SampleControllerTest.java
+++ src/test/java/me/whiteship/demobootweb/SampleControllerTest.java
@@ -6,9 +6,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -29,5 +31,13 @@ class SampleControllerTest {
                         .param("id", savedPerson.getId().toString()))
                 .andDo(print())
                 .andExpect(content().string("hello keesun"));
+    }
+
+    @Test
+    void helloStatic() throws Exception {
+        this.mockMvc.perform(get("/index.html"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("hello index")));
     }
 }
