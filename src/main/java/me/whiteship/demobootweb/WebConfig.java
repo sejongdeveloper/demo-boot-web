28a81d16b8334da65c25ce4aa316fdbac2c2228a
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-14 13:49:52 +0900

    핸들러 인터셉터 구현

--- src/main/java/me/whiteship/demobootweb/WebConfig.java
+++ src/main/java/me/whiteship/demobootweb/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new GreetingInterceptor());
-        registry.addInterceptor(new AnotherInterceptor());
+        registry.addInterceptor(new GreetingInterceptor()).order(0);
+        registry.addInterceptor(new AnotherInterceptor()).order(-1);
     }
 }
