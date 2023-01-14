8e48f08e34481bc8d4377822f36aec6d514aadf1
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-14 13:44:44 +0900

    핸들러 인터셉터

--- src/main/java/me/whiteship/demobootweb/SampleController.java
+++ src/main/java/me/whiteship/demobootweb/SampleController.java
@@ -7,6 +7,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SampleController {
 
+    // preHandle 1
+    // preHandle 2
+    // 요청 처리
+    // postHandle 2
+    // postHandle 1
+    // 뷰 랜더링
+    // afterCompletion 2
+    // afterCompletion 1
     @GetMapping("/hello")
     public String hello(@RequestParam("id") Person person) {
         return "hello " + person.getName();
